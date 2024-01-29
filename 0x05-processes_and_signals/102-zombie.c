73d4adc126694c0b31590a907a387d700bf5bc2a
gehad <gehadgamalazzam@gmail.com>
2024-01-29 22:55:47 +0200

    manage

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,34 @@
-#!/usr/bin/env bash
+#include <sys/types.h>
+#include<stdio.h>
+#include "unistd.h"
+/**
+*infinite_while - infiniteloop
+*Return: 0 or 1
+*/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+*main - entry main
+*Return: always 0
+*/
+int main()
+{
+	pid_t proc_id;
+	int count = 1;
+	while (count <= 5)
+{
+		proc_id = fork();
+		if (!proc_id)
+			return (0);
+		printf("Zombie process created, proc_id: %d\n", proc_id);
+		count++;
+}
+	infinite_while();
+	return (0);
+}
