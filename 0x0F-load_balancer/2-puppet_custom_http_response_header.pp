feabee059c661bb289dacde0f9b2dc2ea4e0eacf
gehad <gehadgamalazzam@gmail.com>
2024-04-02 02:05:18 +0200

    pubbet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,23 @@
 #automate the task of creating a custom HTTP header response, but with Puppet.
+exec { 'update_system':
+    command => '/usr/bin/apt-get update',
+}
+
+-> package { 'nginx':
+    ensure  => 'present',
+    require => Exec['update_system']
+}
+
+file {'/var/www/html/index.html':
+    content => 'Hello World!'
+}
+
+-> file_line { 'add_custom_header':
+    path  => '/etc/nginx/nginx.conf',
+    match => 'http {',
+    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+
+-> exec {'nginx_run':
+    command => '/usr/sbin/service nginx restart',
+}
