222f1a830306ef8468a2917b49f067b0d607fdff
gehad <gehadgamalazzam@gmail.com>
2024-05-07 14:46:26 +0300

    Task 2 recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """ queries the Reddit API and
-returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit
+returns a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit
 the function should return None."""
 
 
 import requests as rq
 
-def recurse(subreddit, hot_list=[]):
+
+def recurse(subreddit, hot_list=[], counter=""):
     """If not a valid subreddit, return None."""
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = rq.get(url, headers=headers)
+    parmeters = {"after": counter}
+    response = rq.get(url, headers=headers, params=parmeters)
 
     if response.status_code != 200:
         return None
+
+    else:
+        for post in response.json()["data"]["children"]:
+            hot_list.append(post["data"]["title"])
+            counter = response.json()["data"]["after"]
+            if counter is not None:
+                recurse(subreddit, hot_list, counter)
+            else:
+                return hot_list
