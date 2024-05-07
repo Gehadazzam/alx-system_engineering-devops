dcd158c62ba09f455b96bd74f97ad0a9ba74b56a
gehad <gehadgamalazzam@gmail.com>
2024-05-07 15:25:55 +0300

    update task 0 no redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Custom"}
-    response = rq.get(url, headers=headers)
+    response = rq.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
